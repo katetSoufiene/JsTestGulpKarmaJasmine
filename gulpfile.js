5424469ba281cd2a66bc24e3ccbf271959c3bb31
soufiene <soufiene.katet@outlook.com>
2020-02-08 12:47:56 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+﻿
+
+var gulp = require('gulp');
+var karma = require('karma').Server;
+
+gulp.task('test', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: false
+    }, function () {
+        done();
+    });
+});
+
+
+
+
---
+++ spec/Example.spec.js
@@ -0,0 +1,12 @@
+﻿
+
+describe("Exemple", function () {
+
+
+    it("should be true", function () {
+        var test = true;
+        expect(test).toBe(true);
+    });
+
+});
+
